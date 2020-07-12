5845f73a00d30f713079a678189187f96d40517a
Martin Yin <qwqqw155@gmail.com>
2020-07-12 22:50:35 +0800

    Added SESSION in login function

--- springboot-login/src/main/java/com/Kinghao/controller/UserController.java
+++ springboot-login/src/main/java/com/Kinghao/controller/UserController.java
@@ -7,13 +7,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -28,6 +28,7 @@ public class UserController {
      * @return Result
      */
     @PostMapping(value = "/regist")
+    @ResponseBody
     public Result regist(User user){
         logger.trace("login was request");
         return userService.regist(user);
@@ -39,9 +40,21 @@ public class UserController {
      * @return Result
      */
     @PostMapping(value = "/login")
+    @ResponseBody
     public Result login(User user, HttpServletRequest request){
         logger.trace("login was request");
-        return userService.login(user);
+        Result R1=userService.login(user);
+        if(R1.isSuccess()){
+            HttpSession session=request.getSession();
+            session.setAttribute("user",user);
+            R1.setMsg(user.getUsername()+", Hello!");
+        }
+        return R1;
+    }
+    @PostMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "login";
     }
 }
 
